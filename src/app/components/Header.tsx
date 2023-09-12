55a43668f13e96d0b45c65cba444ae886c972c2c
huy8856 <thai.giahuy123@gmail.com>
2023-09-12 21:18:13 +0700

    Header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+type Props = {}
+export default function Header({}: Props) {
+	return (
+		<header>
+			<section>
+				<div>LOGO</div>
+				<div>Menu 1</div>
+				<div>Menu 2</div>
+				<div>SEARCH BAR</div>
+			</section>
+		</header>
+	)
+}
