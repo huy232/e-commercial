97579c9e76abeb4b39006ef98efbc6800b001ad4
huy8856 <thai.giahuy123@gmail.com>
2023-10-05 23:17:18 +0700

    Try to add cartlist, prototype

--- src/components/Navbar/Cart.tsx
+++ src/components/Navbar/Cart.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from "react"
 import { AiOutlineShoppingCart } from "react-icons/ai"
 import classNames from "classnames"
 import CartList from "./CartList"
@@ -9,15 +11,19 @@ type Props = {
 
 const Cart = (props: Props) => {
 	const { itemCount = "99+", itemInfo } = props
+
 	const cartClassName = classNames(
 		"absolute flex items-center justify-center right-[2px] bottom-[-10px] rounded-full bg-red-500 w-[30px] h-[30px] text-[12px] font-bold transition-all duration-200 cursor-default",
 		itemCount ? "opacity-100" : "opacity-0"
 	)
+
 	return (
-		<div className="relative mx-2">
-			<AiOutlineShoppingCart className="text-3xl mx-2" />
-			<p className={cartClassName}>{itemCount}</p>
-			<CartList />
+		<div className={"relative"}>
+			<div className="relative mx-2">
+				<AiOutlineShoppingCart className="text-3xl mx-2" />
+				<p className={cartClassName}>{itemCount}</p>
+			</div>
+			{<CartList />}
 		</div>
 	)
 }
--- src/components/Navbar/CartList.tsx
+++ src/components/Navbar/CartList.tsx
@@ -1,5 +1,9 @@
 type Props = {}
 const CartList = (props: Props) => {
-	return <div>CartList</div>
+	return (
+		<div className="absolute h-[300px] w-[260px] m-4 p-2 bg-slate-500">
+			CartList
+		</div>
+	)
 }
 export default CartList
